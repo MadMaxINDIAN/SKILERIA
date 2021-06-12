ab82a34ea74d46b40202a419f28ed9074b870970
MadMaxINDIAN <56634706+MadMaxINDIAN@users.noreply.github.com>
2021-06-12 11:09:56 +0530

    [IMP] google user login

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import store from "./store";
 import './App.css';
 import Home from './screen/Home';
+import Loader from "./components/Loader";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route exact={true} path="/" component={Home} />
         </div>
       </Router>
+      <Loader />
     </Provider>
   );
 }
--- client/src/screen/Home.js
+++ client/src/screen/Home.js
@@ -1,14 +1,7 @@
 import Navbar from "../components/Navbar";
 import {
     BrowserView,
-    MobileView,
-    isBrowser,
-    isMobile,
-    MobileOnlyView,
-    isMobileOnly,
-    TabletView
   } from "react-device-detect";
-import Loader from "../components/Loader";
 
 function App() {
   return (
@@ -16,15 +9,7 @@ function App() {
         <Navbar page='home' ></Navbar>
         <BrowserView>
             <div className='margin-top-120'></div>
-            <img src='tab_banner.png' className='mobile-banner' alt='Langing page banner'></img>
         </BrowserView>
-        <MobileOnlyView>
-            <img src='tab_banner.png' className='mobile-banner' alt='Langing page banner'></img>
-        </MobileOnlyView>
-        <TabletView>
-            <img src='tab_banner.png' className='mobile-banner' alt='Langing page banner'></img>
-        </TabletView>
-        <Loader />
     </div>
   );
 }
